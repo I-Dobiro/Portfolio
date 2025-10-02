fa3a255063d35d9db183ae4b25cc08448187f440
Izk <97883727+I-Dobiro@users.noreply.github.com>
2025-10-02 13:09:55 +0100

    holiday booking app update

--- holiday-booking-app/src/App.jsx
+++ holiday-booking-app/src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
+import AddJobPage from './pages/AddJobPage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage />} />
+    </Route>
+      )
+      )
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  )
+      <RouterProvider router={router} />
+      )
 }
 
-export default App
+      export default App
