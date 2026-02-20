0911fbeb47eff834f52ab1aedaefff672029108f
Izk <97883727+I-Dobiro@users.noreply.github.com>
2026-02-20 00:01:03 +0000

    react update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { render } from 'react-dom'
-import App from './App'
-import { ThemeProvider } from './contexts/theme'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider } from './contexts/theme';
+import './index.css';
 
-render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
