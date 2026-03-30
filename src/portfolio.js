757778a89b834e1ae21d91a10f76d9ee2586bf56
Izk <97883727+I-Dobiro@users.noreply.github.com>
2026-03-30 22:07:20 +0100

    updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,13 +6,12 @@ const header = {
 const about = {
     name: 'Isaac Dobiro',
     role: 'Software Developer',
-    // picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
     picture: 'https://plus.unsplash.com/premium_photo-1714618831065-8e8dadd8d3df?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVjaG5vbG9neSUyMGJhY2tncm91bmQlMjBzb2Z0d2FyZSUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww',
     description: 'Full-Stack Developer passionate about creating fast, scalable, and user-focused web applications',
     resume: 'https://example.com',
     social: {
-        // linkedin: 'https://linkedin.com',
-        github: 'https://I-Dobiro.github.io/Portfolio',
+        linkedin: '',
+        github: 'https://github.com/I-Dobiro',
     },
 }
 
@@ -26,24 +25,22 @@ const projects = [
         image: '',
     },
     {
-        name: 'Bugeting App',
+        name: 'Budgeting App',
         description: '',
-        stack: ['HTML', 'CSS', 'JavaScript', 'React'],
-        sourceCode: 'https://github.com',
-        livePreview: 'https://github.com',
+        stack: ['HTML', 'CSS', 'JavaScript', 'Next.js', 'Postgresql (via Neon)'],
+        sourceCode: 'https://github.com/I-Dobiro/budgeting-app',
+        livePreview: 'https://budgeting-app-qx8k.onrender.com/',
         image: '',
     },
     {
         name: 'Job Search App',
         description: '',
-        stack: ['HTML', 'CSS', 'JavaScript', 'React'],
-        sourceCode: 'https://github.com',
-        livePreview: 'https://github.com',
+        stack: ['HTML', 'CSS', 'JavaScript', 'React', 'Express', 'Node', 'Postgresql (via Neon)'],
+        sourceCode: 'https://github.com/I-Dobiro/jobBoard',
+        livePreview: 'https://jobboard-l2ho.onrender.com/',
         image: '',
     },
 ]
-
-// tech store on render https://techstore-becw.onrender.com/
 
 const skills = [
     'HTML',
