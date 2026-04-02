0e713ba4b9fe6cae1ec7fec62df212790680745d
Izk <97883727+I-Dobiro@users.noreply.github.com>
2026-04-02 15:23:34 +0100

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,46 @@
-import { contact } from '../../portfolio'
+import { useRef, useState } from 'react'
+import emailjs from 'emailjs-com'
 import './Contact.css'
 
 const Contact = () => {
-    if (!contact.email) return null
+    const form = useRef()
+    const [status, setStatus] = useState('')
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+        setStatus('sending')
+
+        emailjs
+            .sendForm(
+                'service_p9tfeib',
+                'template_p9z260j',
+                form.current,
+                'uX0iuAdn7liOeld5G'
+            )
+            .then(() => {
+                setStatus('success')
+            })
+            .catch(() => {
+                setStatus('error')
+            })
+    }
 
     return (
-        <section className='section contact center' id='contact'>
-            <h2 className='section__title'>Contact</h2>
-            <a href={`mailto:${contact.email}`}>
-                <span type='button' className='btn btn--outline'>
-                    Email me
-                </span>
-            </a>
+        <section className='contact' id='contact'>
+            <h2>CONTACT</h2>
+
+            <form ref={form} onSubmit={sendEmail} className='contact__form' >
+                <input name='user_name' placeholder='Your Name' required />
+                <input name='user_email' type='email' placeholder='Your Email' required />
+                <textarea name='message' placeholder='Your Message' required />
+
+                <button disabled={status === 'sending'}>
+                    {status === 'sending' ? 'Sending...' : 'Send Message'}
+                </button>
+
+                {status === 'success' && <p className='success'>Message sent!</p>}
+                {status === 'error' && <p className='error'>Something went wrong.</p>}
+            </form>
         </section>
     )
 }
--- src/portfolio.js
+++ src/portfolio.js
@@ -8,7 +8,7 @@ const about = {
     role: 'Software Developer',
     picture: 'https://plus.unsplash.com/premium_photo-1714618831065-8e8dadd8d3df?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVjaG5vbG9neSUyMGJhY2tncm91bmQlMjBzb2Z0d2FyZSUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww',
     description: 'Full-Stack Developer passionate about creating fast, scalable, and user-focused web applications',
-    resume: 'https://example.com',
+    resume: '',
     social: {
         linkedin: '',
         github: 'https://github.com/I-Dobiro',
@@ -18,7 +18,7 @@ const about = {
 const projects = [
     {
         name: 'E-Commerce Store',
-        description: '',
+        description: 'This is a full-stack e-commerce application built with React for the frontend and Node.js with Express for the backend. The application allows users to browse products, login securely,add products for sale, and add comments. The backend handles user authentication, product management, and order processing. The database is managed using PostgreSQL via Neon and Drizzle, ensuring efficient data storage and retrieval.',
         stack: ['HTML', 'CSS', 'JavaScript', 'Typescript', 'React', 'Node', 'Express', 'Postgresql (via Neon & Drizzle)'],
         sourceCode: 'https://github.com/I-Dobiro/e-commerce-store',
         livePreview: 'https://e-commerce-store-vw9d.onrender.com/',
@@ -26,15 +26,15 @@ const projects = [
     },
     {
         name: 'Budgeting App',
-        description: '',
-        stack: ['HTML', 'CSS', 'JavaScript', 'Next.js', 'Postgresql (via Neon)'],
-        sourceCode: 'https://github.com/I-Dobiro/budgeting-app',
-        livePreview: 'https://budgeting-app-qx8k.onrender.com/',
+        description: '{ Under Construction } I am Currently building a budgeting app to help users manage their finances effectively. The app will allow users to track their income and expenses, set budgets, and visualize their financial data through charts and graphs with AI integrated for financial insights and recommendations for users. I am using HTML, CSS, JavaScript, Next.js for the frontend, and Postgresql (via Neon & Prisma) for the backend database.',
+        stack: ['HTML', 'CSS', 'JavaScript', 'Next.js', 'Postgresql (via Neon & Prisma)'],
+        // sourceCode: 'https://github.com/I-Dobiro/budgeting-app',
+        // livePreview: 'https://budgeting-app-qx8k.onrender.com/',
         image: '',
     },
     {
         name: 'Job Search App',
-        description: '',
+        description: 'This is a full stack job search application built with React for the frontend and Node.js with Express for the backend. The application allows users to search for jobs, view job details, edit, and delete jobs. The backend handles job listings. The database is managed using PostgreSQL via Neon, ensuring efficient data storage and retrieval.',
         stack: ['HTML', 'CSS', 'JavaScript', 'React', 'Express', 'Node', 'Postgresql (via Neon)'],
         sourceCode: 'https://github.com/I-Dobiro/jobBoard',
         livePreview: 'https://jobboard-l2ho.onrender.com/',
